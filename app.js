740ba4dcf3f34ece3ec1e5cf4a57b8f75ebacac3
Sid-Hughes <58391103+Sid-Hughes@users.noreply.github.com>
2024-09-13 13:25:19 +0100

    fix: fixed sliding window of images on top

--- app.js
+++ app.js
@@ -7,22 +7,34 @@ document.addEventListener("DOMContentLoaded", () => {
             image.src = data.message;
             image.width = 100;
             image.height = 100;
-        })
-    } 
-})
+        });
+    }
 
-var imgs = document.querySelectorAll('.slider img')
-var dots = document.querySelectorAll('.dot')
-var currentImg = 0;
+
+    
+
+});
+
 const interval = 3000;
-
+var currentImg = 0;
 function changeSlide(n) {
+    var imgs = document.querySelectorAll('.slider img')
+    var dots = document.querySelectorAll('.dot')
+    
+    console.log(imgs.length)
+    console.log(dots.length)
     for (var i =0; i<imgs.length; i++){
         imgs[i].style.opacity = 0;
         dots[i].className = dots[i].className.replace(' active', '')
     }
-    currentImg = n;
-
+    currentImg = (currentImg + 1) % imgs.length;
+    if (n != undefined){
+        clearInterval(timer);
+        timer = setInterval(changeSlide, interval);
+        currentImg = n;
+    }
     imgs[currentImg].style.opacity = 1;
     dots[currentImg].className += ' active';
 }
+
+var timer = setInterval(changeSlide, interval);
